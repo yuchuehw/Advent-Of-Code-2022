93ac87c7b0ad27c21142d6fd222d692be8859fff
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-23 14:24:04 -0800

    Update day1.py

--- src/day1.py
+++ src/day1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-with open(sys.argv[1], "r", encoding="utf8") as f:
+with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8") as f:
     f = f.read()
     print(max([sum((*map(int, group.split("\n")),)) for group in f.split("\n\n")]))
     print(
