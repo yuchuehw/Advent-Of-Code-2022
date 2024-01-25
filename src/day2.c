dbdb86fd8272307ed75b831bc03f5865c2a68c50
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-25 14:33:13 -0800

    Update day2.c

--- src/day2.c
+++ src/day2.c
@@ -25,14 +25,13 @@ bool fileOpener(int argc, char *argv[], FILE **file) {
 }
 
 /**
- * Reads values from a file, updates the top 3 values, and handles errors.
+ * Reads values from a file, tally result and score for each game using rules in part 1.
  *
  * @param file The FILE pointer for the opened file.
  * @param score Pointer to an integer representing the score.
  * @return true if the result is successfully tallied, false otherwise.
  */
-bool tally_result(FILE *file, int *score) {
-    int current_group_sum = 0;
+bool tally_part1(FILE *file, int *score) {
     char buf[5];
     char *end;
     while (fgets(buf, sizeof buf, file) != NULL) {
@@ -58,12 +57,47 @@ bool tally_result(FILE *file, int *score) {
 }
 
 /**
+ * Reads values from a file, tally result and score for each game using rules in part 2.
+ *
+ * @param file The FILE pointer for the opened file.
+ * @param score Pointer to an integer representing the score.
+ * @return true if the result is successfully tallied, false otherwise.
+ */
+bool tally_part2(FILE *file, int *score) {
+    char buf[5];
+    char *end;
+    while (fgets(buf, sizeof buf, file) != NULL) {
+        if(buf[0] != 'A' && buf[0] != 'B' && buf[0] != 'C'){
+            printf("Invalid character detected: %c\n", buf[0]);
+            return false;
+        }
+        int a = buf[0] - 'A';
+        *score += 1;
+        if(buf[2] == 'X'){
+            *score += (a+2)%3;
+        }else if(buf[2] == 'Y'){
+            *score += a;
+            *score += 3;
+        }else if(buf[2] == 'Z'){
+            *score += (a+1)%3;
+            *score += 6;
+        }
+        else{
+            printf("Invalid character detected: %c\n", buf[2]);
+            return false;
+        }
+
+    }
+    return true;
+}
+
+/**
  * Prints the top 1 value and the sum of the top 3 values.
  *
  * @param score Integer representing the score.
  */
 void report_result(int score) {
-    printf("Your score is: %i\n", score);
+    printf("%i\n", score);
 }
 
 int main(int argc, char *argv[]) {
@@ -73,13 +107,20 @@ int main(int argc, char *argv[]) {
     }
 
     int score = 0;
-    if (!tally_result(file, &score)) {
+    if (!tally_part1(file, &score)) {
         fclose(file);
         return 1;
     }
-    fclose(file);
-
     report_result(score);
+    score = 0;
+    fseek(file, 0, SEEK_SET);
+    if (!tally_part2(file, &score)) {
+        fclose(file);
+        return 1;
+    }
+    report_result(score);
+
+    fclose(file);
 
     return 0;
 }
