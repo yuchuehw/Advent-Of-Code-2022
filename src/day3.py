fd32067505947ad9759747d9239eae322ba5b7f5
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-01-30 15:09:53 -0800

    Update day3.py

--- src/day3.py
+++ src/day3.py
@@ -3,7 +3,8 @@ import sys
 
 with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8") as f:
     sum = 0
-    for line in f.read().split("\n"):
+    f=f.read().split("\n")
+    for line in f:
         c = set(line[: len(line) // 2]).intersection(set(line[len(line) // 2 :])).pop()
         if c.isupper():
             sum += 27
@@ -11,5 +12,16 @@ with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8
         else:
             sum += 1
             sum += ord(c) - ord("a")
-
+    print(sum)
+    sum = 0
+    for l1,l2,l3 in zip(f[::3],f[1::3],f[2::3],strict=1):
+        c = (set(l1).intersection(set(l2))
+                    .intersection(set(l3)).pop())
+        if c.isupper():
+            sum += 27
+            sum += ord(c) - ord("A")
+        else:
+            sum += 1
+            sum += ord(c) - ord("a")
+    
     print(sum)
