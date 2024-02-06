dc2647dd770cd742042990bbea078b4c94f75ed8
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-05 20:57:40 -0800

    Update day5.py

--- src/day5.py
+++ src/day5.py
@@ -8,7 +8,7 @@ with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8
     Instrction = namedtuple("Instruction", "quantity origin destination")
     L = ""
     for line in f.read().split("\n"):
-        if not L and not set(line) - set(" 0123456789"):
+        if not L and line and line[0]==" ":
             L = line
         elif L and line:
             _, quantity, _, origin, _, destination = line.split()
@@ -18,11 +18,18 @@ with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8
                 if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
                     d1[i].insert(0, c)
     d2 = {}
+    d3 = {}
     for o in L.split():
-        d2[o] = d1[L.index(o)]
+        d2[o] = d1[L.index(o)][:]
+        d3[o] = d1[L.index(o)][:]
     for quantity, origin, destination in instructions:
         for _ in range(quantity):
             d2[destination].append(d2[origin].pop())
+        d3[destination] += d3[origin][-quantity:]
+        d3[origin] = d3[origin][:-quantity]
     for i in sorted(d2):
         print(d2[i].pop(), end="")
+    print()
+    for i in sorted(d3):
+        print(d3[i].pop(), end="")
     print()
