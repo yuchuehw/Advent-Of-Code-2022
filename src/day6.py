4a90c32b8fc3f6abb6c2eccbbceeff0395e0d311
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-02-06 10:48:42 -0800

    Create day6.py

---
+++ src/day6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+
+def detect_packet(message: str, packet_lenght: int):
+    for i in range(len(message)):
+        if len(set(message[i : i + packet_lenght])) == packet_lenght:
+            return i + packet_lenght
+
+
+with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8") as f:
+    f = f.read()
+    print(detect_packet(f, 4))
