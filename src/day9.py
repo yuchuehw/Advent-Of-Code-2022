6e776e6c8a90e82c54dbe8036915fd237f0d1294
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-19 12:03:07 -0700

    Update day9.py

--- src/day9.py
+++ src/day9.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
+import sys
+from typing import List, Tuple
 import numpy as np
 
-instruct="""R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2"""
 
-head = np.array((0, 0))
-tail = np.array((0, 0))
-visited = {(0, 0)}
-
-direction_to_change = {
+DIRECTION_TO_CHANGE = {
     "U": np.array((0, 1)),
     "D": np.array((0, -1)),
     "L": np.array((-1, 0)),
     "R": np.array((1, 0)),
 }
 
-for row in instruct.split("\n"):
-    direction, step = row.split()
-    for _ in range(int(step)):
-        temp = tuple(head)
-        head += direction_to_change[direction]
-        if tuple(head) not in [tuple(tail + (x, y)) for x in range(-1, 2) for y in range(-1, 2)]:
-            tail = np.array(temp)
-            visited.add(temp)
 
-print(len(visited))
+def find_visited(
+    nodes: List[np.ndarray[Tuple[int, int]]], instruct: str, visited: set
+) -> None:
+    assert len(nodes) >= 2 and "there must be at least two nodes"
+    for row in instruct.split("\n"):
+        direction, step = row.split()
+        for _ in range(int(step)):
+            nodes[0] += DIRECTION_TO_CHANGE[direction]
+            previous_node = nodes[0]
+            for i, node in enumerate(nodes):
+                if not i:
+                    continue
+                if any(abs(previous_node - node) > 1):
+                    delta = [
+                        -1 if i < 0 else 1 if i else 0 for i in (previous_node - node)
+                    ]
+                    node += delta
+                    previous_node = node
+                else:
+                    break
+            visited.add(tuple(nodes[-1]))
+
+
+def main():
+    filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+    with open(filename, "r", encoding="utf8") as f:
+        instruct = f.read()
+    visited = {(0, 0)}
+    find_visited([np.array((0, 0)) for _ in range(2)], instruct, visited)
+    print(len(visited))
+
+    visited = {(0, 0)}
+    find_visited([np.array((0, 0)) for _ in range(10)], instruct, visited)
+    print(len(visited))
+
+
+if __name__ == "__main__":
+    main()
