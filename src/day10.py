235624dec5e3c5206afa91ce76ef82eb25ae9a34
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-22 12:44:15 -0700

    Update day10.py

--- src/day10.py
+++ src/day10.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
 import sys
-import tempfile
 
-with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8") as f:
-    instructions = f.read()
 
-value = 1
-old_value = 1
+def print_(cycle: int, value: int, p2_answer: list):
+    if value - 1 <= (cycle - 1) % 40 <= value + 1:
+        p2_answer.append("#")
+    else:
+        p2_answer.append(".")
+    if cycle and cycle % 40 == 0:
+        p2_answer.append("\n")
+    if cycle and (cycle - 20) % 40 == 0:
+        return cycle * value
+    return 0
 
-cycle = 1
 
-answer = 0
+def main():
 
-with tempfile.TemporaryFile() as tmp:
+    with open(
+        sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8"
+    ) as f:
+        instructions = f.read()
 
-    def print_(cycle, value):
-        if value - 1 <= (cycle - 1) % 40 <= value + 1:
-            tmp.write(b"#")
-        else:
-            tmp.write(b".")
-        if cycle and cycle % 40 == 0:
-            tmp.write(b"\n")
-        if cycle and (cycle - 20) % 40 == 0:
-            return cycle * value
-        return 0
+    value = 1
+
+    cycle = 1
+
+    p1_answer = 0
+    p2_answer = []
 
     for i in instructions.split("\n"):
-        answer += print_(cycle, value)
+        p1_answer += print_(cycle, value, p2_answer)
         if " " not in i:
             cycle += 1
         else:
             cycle += 1
-            answer += print_(cycle, value)
+            p1_answer += print_(cycle, value, p2_answer)
             cycle += 1
-            value += int(i[i.index(" ") + 1:])
+            value += int(i[i.index(" ") + 1 :])
 
-    print(answer)
-    tmp.seek(0)
-    print(str(tmp.read(), "utf-8"))
+    print(p1_answer)
+
+    print("".join(p2_answer))
+
+
+if __name__ == "__main__":
+    main()
