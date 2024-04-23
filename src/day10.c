27ed0d712b55571b9476aec62788cab3c830cefc
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-23 08:04:40 -0700

    Update day10.c

--- src/day10.c
+++ src/day10.c
@@ -7,14 +7,14 @@ typedef struct CharNode {
 } CharNode;
 
 typedef struct HeadNode {
-	int i;
-	CharNode *next;
+  int i;
+  CharNode *next;
 } HeadNode;
 
 void freeLinkList(HeadNode *head) {
-	CharNode *linkList = head->next;
-	free(head);
-	while (linkList != NULL) {
+  CharNode *linkList = head->next;
+  free(head);
+  while (linkList != NULL) {
     CharNode *temp = linkList;
     linkList = linkList->next;
     free(temp);
@@ -36,67 +36,76 @@ int fileOpener(int argc, char *argv[], FILE **file) {
   return 1;
 }
 
-int extendLinkList(int cycle, int value, HeadNode *head){
-	char new_c = '.';
-	if (value - 1 <= (cycle - 1) % 40 && (cycle - 1) % 40 <= value + 1){
-		new_c = '#';
-	}
-
-	CharNode *node = malloc(sizeof(CharNode));
-	node->c = new_c;
-	node->next = NULL;
-	if (head->next == NULL){
-		head->next = node;
-	} else{
-		CharNode *linkList = head->next;
-		while (linkList->next){
-			linkList = linkList->next;
-		}
-		linkList->next = node;
-		if (cycle && cycle % 40 == 0){
-			CharNode *newLine = malloc(sizeof(CharNode));
-			node->next = newLine;
-			newLine->c = '\n';
-			newLine->next = NULL;
-		}
-	}
-
-	if (cycle && (cycle - 20) % 40 == 0){
-		return cycle * value;
-	}
-	return 0;
-}
-
-HeadNode* simulate(FILE *file){
-	HeadNode *head = malloc(sizeof(HeadNode));
-	head->next = NULL;
-	int value = 1;
-	int cycle = 1;
-
-	int answer = 0;
-
-	char buf[20];
-	while (fgets(buf, sizeof buf, file) != NULL) {
-    answer += extendLinkList(cycle, value, head);
-		if (buf[0] == 'n'){
-			cycle += 1;
-		}else{
-			cycle += 1;
-			answer += extendLinkList(cycle, value, head);
-			cycle += 1;
-			value += atoi(buf+5);
-		}
+int extendLinkList(int cycle, int value, HeadNode *head) {
+  char new_c = '.';
+  if (value - 1 <= (cycle - 1) % 40 && (cycle - 1) % 40 <= value + 1) {
+    new_c = '#';
   }
 
-	head->i = answer;
-	return head;
-} 
+  CharNode *node = malloc(sizeof(CharNode));
+  node->c = new_c;
+  node->next = NULL;
+  if (head->next == NULL) {
+    head->next = node;
+  } else {
+    CharNode *linkList = head->next;
+    while (linkList->next) {
+      linkList = linkList->next;
+    }
+    linkList->next = node;
+    if (cycle && cycle % 40 == 0) {
+      CharNode *newLine = malloc(sizeof(CharNode));
+      node->next = newLine;
+      newLine->c = '\n';
+      newLine->next = NULL;
+    }
+  }
+
+  if (cycle && (cycle - 20) % 40 == 0) {
+    return cycle * value;
+  }
+  return 0;
+}
+
+HeadNode *simulate(FILE *file) {
+  HeadNode *head = malloc(sizeof(HeadNode));
+  head->next = NULL;
+  int value = 1;
+  int cycle = 1;
+
+  int answer = 0;
+
+  char buf[20];
+  while (fgets(buf, sizeof buf, file) != NULL) {
+    answer += extendLinkList(cycle, value, head);
+    if (buf[0] == 'n') {
+      cycle += 1;
+    } else {
+      cycle += 1;
+      answer += extendLinkList(cycle, value, head);
+      cycle += 1;
+      value += atoi(buf + 5);
+    }
+  }
+
+  head->i = answer;
+  return head;
+}
 
 int main(int argc, char *argv[]) {
   FILE *file = NULL;
   if (!fileOpener(argc, argv, &file)) {
     return 1;
   }
-	HeadNode *head = simulate(file);
-	printf("%d\n",head->i);
+  HeadNode *head = simulate(file);
+  printf("%d\n", head->i);
+  CharNode *linkList = head->next;
+  while (linkList != NULL) {
+    CharNode *temp = linkList;
+    linkList = linkList->next;
+    printf("%c", temp->c);
+  }
+  printf("\n");
+  freeLinkList(head);
+  fclose(file);
 }
