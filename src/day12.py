b0c8d93c8ce607868f8f829a96777d6f9da6ae94
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-25 09:01:33 -0700

    Update day12.py

--- src/day12.py
+++ src/day12.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import numpy as np
 
 with open(
     sys.argv[1] if len(sys.argv) > 1 else "input.txt", "r", encoding="utf8"
@@ -33,23 +34,23 @@ def bfs(grid, candidates, end, seen):
             if (
                 0 <= ny < len(grid)
                 and 0 <= nx < len(grid[0])
-                and new_point not in seen
+                and not seen[ny][nx]
                 and grid[ny][nx] - 1 <= grid[y][x]
             ):
                 if new_point == end:
                     return len(candidate)
-                seen.add(new_point)
+                seen[ny][nx]=1
                 next_candidates.append(candidate[:] + [new_point])
     return bfs(grid, next_candidates, end, seen)
 
 
-print(bfs(GRID, [[start]], end, set()))
+print(bfs(GRID, [[start]], end, np.zeros_like(GRID)))
 
 
 print(
     min(
         [
-            bfs(GRID, [[(i, j)]], end, set())
+            bfs(GRID, [[(i, j)]], end, np.zeros_like(GRID))
             for i, row in enumerate(GRID)
             for j, c in enumerate(row)
             if c == ord("a")
