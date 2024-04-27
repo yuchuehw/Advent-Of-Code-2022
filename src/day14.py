b9eb409881c86ca8c00a9942df2cf6c3efb2fa04
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-04-27 11:33:21 -0700

    Update day14.py

--- src/day14.py
+++ src/day14.py
@@ -1,5 +1,10 @@
-data = """498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9"""
+#!/usr/bin/env python3
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+with open(filename, "r", encoding="utf8") as f:
+    data = f.read().strip()
 
 occupied = set()
 
@@ -14,26 +19,31 @@ for row in data.split("\n"):
         range1 = range(p1[0],p2[0]+1) if p1[0]<p2[0]else range(p2[0],p1[0]+1)
         range2 = range(p1[1],p2[1]+1) if p1[1]<p2[1]else range(p2[1],p1[1]+1)
         occupied.update({(x,y)for x in range1 for y in range2})
-    
-    
+
 sand = 0
+
+START_POSITION = (500,-1)
+BLOCKING_POSITION = (500,0)
+
+found = 0
 while True:
     sand += 1
-    current_position = (500,-1)
-    found = False
+    current_position = START_POSITION
     while True:
-        targets = [(current_position[0]+dx,current_position[1]+dy) for dx, dy in [(0,1),(-1,1),(1,1)]]
+        targets = [(current_position[0]+dx,current_position[1]+dy) for dx, dy in [(0,1),(-1,1),(1,1)] if current_position[1]<=LARGEST_Y]
         for target in targets:
             if target not in occupied:
                 current_position = target
                 break
         else:
             occupied.add(current_position)
+            if (current_position == BLOCKING_POSITION):
+                print(sand)
+                found = 2 
             break
-        if target[1] > LARGEST_Y:
-            found = True
-            break
+        if target[1] > LARGEST_Y and not found:
+            found = 1
+            print(sand-1)
     
-    if found:
-        print(sand-1)
+    if found == 2:
         break
